43818e48acb4d950a3b59abb2b7e688f89365318
itshsinyu9876 <sa333559@gmail.com>
2022-06-06 12:36:12 +0800

    calc avg of reviews

--- read.py
+++ read.py
@@ -4,9 +4,11 @@ with open('reviews.txt','r') as f:
     for line in f:
         data.append(line)
         count +=1
-        if count % 1000 == 0: # %是用來求餘數的
+        if count % 100000 == 0: # %是用來求餘數的
             print(len(data))
-print(len(data))
-print(data[0])
-print('--------------') #方便區分留言
-print(data[1])
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+sum_len = 0
+for comment in data:
+    sum_len += len(comment) # =sum_len+len(comment) 算出總長度
+print('留言的平均長度為', sum_len/len(data)) 
